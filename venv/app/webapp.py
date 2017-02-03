36e4221d1b1f4364d2451faac417d5cba8381576
uria17-meet <uria17@meet.mit.edu>
2017-02-03 16:12:12 +0200

    orginaized

---
+++ venv/app/webapp.py
@@ -0,0 +1,20 @@
+from flask import *
+from model import *
+
+
+app = Flask(__name__)
+
+engine = create_engine('sqlite:///Database.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+
+@app.route('/')
+def landing_page():
+    return "hello"
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True,)
